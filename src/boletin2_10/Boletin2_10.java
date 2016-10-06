95c1133203676878b652773421e58aed76b8c79f
agarridogarcia <agarridogarcia@atanasoff30.danielcastelao.org>
2016-10-06 10:03:10 +0200

    Cantidad entera a billetes de 100, 20, 5 y monedas de 1

--- src/boletin2_10/Boletin2_10.java
+++ src/boletin2_10/Boletin2_10.java
@@ -8,8 +8,8 @@ public class Boletin2_10 {
 
     
     public static void main(String[] args) {
-        //metemos una cantidad entera y nos la divide entre billetes de 100, 20, 5 y monedas.
-        int cantidadB100,cantidadB20, cantidadB5,cantidad, monedas;
+        //metemos una cantidad entera y nos la divide entre billetes de 100€, 20€, 5€ y monedasde 1€.
+        int cantidadB100,cantidadB20, cantidadB5,cantidad, monedas; 
         cantidad=Integer.parseInt(JOptionPane.showInputDialog("Introduzca el dinero que tiene"));
         cantidadB100=cantidad/100;
         cantidadB20=cantidad%100/20;
